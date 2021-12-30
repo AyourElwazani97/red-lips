ebeb0c76a0380c7b41c3cff987e1e39386537fc8
your_name <your_address_email@domain.com>
2021-12-30 18:14:31 +0100

    adding reviews section

--- components/Reviews/Reviews.js
+++ components/Reviews/Reviews.js
@@ -1,37 +1,140 @@
 import React, { useEffect, useRef } from "react";
-import * as THREE from "three";
+import gsap, { Expo } from "gsap";
+import ScrollTrigger from "gsap/dist/ScrollTrigger";
+import { FaLongArrowAltRight } from "react-icons/fa";
 function Reviews({ styles }) {
-  const mount = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
+  const reviews_parent = useRef(null);
+  const review = useRef(null);
+  const secondContentRev = useRef(null);
+  const thirdContentRev = useRef(null);
+  const lastContentRev = useRef(null);
   useEffect(() => {
-    var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    );
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    // document.body.appendChild( renderer.domElement );
-    // use ref as a mount point of the Three.js scene instead of the document.body
-    mount.appendChild(renderer.domElement);
-    var geometry = new THREE.BoxGeometry(1, 1, 1);
-    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    var cube = new THREE.Mesh(geometry, material);
-    scene.add(cube);
-    camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame(animate);
-      cube.rotation.x += 0.01;
-      cube.rotation.y += 0.01;
-      renderer.render(scene, camera);
-    };
-    animate();
+    const tl = gsap.timeline({
+      scrollTrigger: {
+        ease: Expo.easeOut,
+        trigger: review.current,
+        toggleActions: "play",
+        start: "top top",
+        end: "=+300%",
+        scrub: 1,
+        pin: true,
+      },
+    });
+    tl.fromTo(
+      reviews_parent.current.children[2],
+      { clipPath: "polygon(0 0, 0 0, 0% 100%, 0% 100%)" },
+      {
+        clipPath: "polygon(0 0 , 100% 0, 100% 100% , 0% 50%)",
+        duartion: 2,
+      }
+    )
+      .to(reviews_parent.current.children[2], {
+        clipPath: "polygon(0 0, 0 0, 0% 100%, 0% 100%)",
+        duartion: 2,
+      })
+      .fromTo(
+        reviews_parent.current.children[0],
+        { clipPath: "polygon(0 0, 0 0, 0% 100%, 0% 100%)" },
+        {
+          clipPath: "polygon(0 0 , 100% 0, 100% 100% , 0% 50%)",
+          duartion: 2,
+        }
+      )
+      .to(reviews_parent.current.children[0], {
+        clipPath: "polygon(0 0, 0 0, 0% 100%, 0% 100%)",
+        duartion: 2,
+      })
+      .fromTo(
+        reviews_parent.current.children[1],
+        { clipPath: "polygon(0 0, 0 0, 0% 100%, 0% 100%)" },
+        {
+          clipPath: "polygon(0 0 , 100% 0, 100% 100% , 0% 50%)",
+          duartion: 2,
+        }
+      )
+      .to(reviews_parent.current.children[1], {
+        clipPath: "polygon(0 0, 0 0, 0% 100%, 0% 100%)",
+        duartion: 2,
+      });
   }, []);
   return (
-    <div ref={(el) => (mount = el)}>
-      <div>
-        <img src="/storyFirstYear.jpg" alt="" />
+    <div ref={review} className={styles._review}>
+      <h1>Testimonial</h1>
+      <div ref={reviews_parent} className={styles._reviews_Child}>
+        <div className={styles._reviews_Child_each}>
+          <div>
+            <img src="./testimonial2.jpg" alt="" />
+          </div>
+          <div
+            ref={secondContentRev}
+            className={styles._reviews_Child_each_textContent}
+          >
+            <div>
+              <h2>
+                Oum
+                <br />
+                Tourîa
+              </h2>
+            </div>
+            <div className={styles._reviews_discover}>
+              <div>
+                <h6>Discover</h6>
+              </div>
+              <div>
+                <span>
+                  <FaLongArrowAltRight />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div ref={thirdContentRev} className={styles._reviews_Child_each}>
+          <div>
+            <img src="./testimonial1.jpg" alt="" />
+          </div>
+          <div className={styles._reviews_Child_each_textContent}>
+            <div>
+              <h2>
+                Oum
+                <br />
+                Tourîa
+              </h2>
+            </div>
+            <div className={styles._reviews_discover}>
+              <div>
+                <h6>Discover</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className={styles._reviews_Child_each}>
+          <div>
+            <img src="./testimonial3.jpg" alt="" />
+          </div>
+          <div
+            ref={lastContentRev}
+            className={styles._reviews_Child_each_textContent}
+          >
+            <div>
+              <h2>
+                Oum
+                <br />
+                Tourîa
+              </h2>
+            </div>
+            <div className={styles._reviews_discover}>
+              <div>
+                <h6>Discover</h6>
+              </div>
+              <div>
+                <span>
+                  <FaLongArrowAltRight />
+                </span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
